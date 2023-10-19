413ad2eab67524cc3f271656c5fc0b112a451d2c
Paul Barretto <paulbaretto827@gmail.com>
2023-10-19 18:13:22 +0530

    initail commit

---
+++ assets/js/main.js
@@ -0,0 +1,109 @@
+
+
+const body = document.querySelector('body');
+const header = document.querySelector('#header-ex-1')
+let searchWrapper = document.querySelector('.nb-search');
+let searchFOrm = searchWrapper.querySelector('form')
+let searchBtn = document.querySelector('#nb-search-open');
+let navMenu = document.querySelector('.nb-collapse-content');
+let navToggle = document.querySelector('.nb-toggler')
+let box = document.querySelector(".nb-collapse-content");
+let menuUl = navMenu.querySelector('ul');
+let menuLi = menuUl.querySelectorAll('li');
+let inputF = document.querySelector('#nb_search')
+console.log(inputF)
+
+// inputF.addEventListener('focus', (e) => {
+// searchFOrm.classList.toggle('hide-me-pls')
+// })
+
+// adding class on scroll *class 1*
+window.addEventListener('scroll', onscroll_function);
+function onscroll_function(){
+    if (window.scrollY > 100){
+        body.classList.add('page-scrolled');
+    } else {
+        body.classList.remove('page-scrolled')
+    }
+}
+// * class 2 *
+window.addEventListener('scroll', onscroll_function02);
+function onscroll_function02(){
+    if (window.scrollY > 250){
+        header.classList.add('header-fixed');
+    } else {
+        header.classList.remove('header-fixed')
+    }
+}
+
+// toggleing a class onclick of search icon to searchWrapper
+searchBtn.addEventListener('click', (e) => {
+    searchWrapper.classList.toggle('search_is_clicked')
+})
+
+// toggleing a class onlick of hamber BTN 
+navToggle.addEventListener('click', (e) => {
+    body.classList.toggle('overflow-y-0')
+    navMenu.classList.toggle('show-nav');
+    let Icon = document.querySelector('#test')
+    console.log(Icon)
+    if (navMenu.classList === 'show-nav'){
+        Icon.className ='fa-solid fa-xmark';
+    }else{
+        Icon.className ='fa-solid fa-bars';
+    }
+        // if(body.classList.contains = 'overflow-y-o'){
+        //     // if(e.target !== navMenu)`
+        //     body.classList.remove('overflow-y-0')
+        //     navMenu.classList.remove('show-nav')
+        // }
+})
+
+// ball move on mouse for nav
+let mouseX = 0;
+let mouseY = 0;
+
+let ballX = 0;
+let ballY = 0;
+
+let speed = 0.09;
+
+
+function animate(){
+  
+  let distX = mouseX - ballX;
+  let distY = mouseY - ballY;
+  
+  
+  ballX = ballX + (distX * speed);
+  ballY = ballY + (distY * speed);
+  
+  ball.style.left = ballX + "px";
+
+  
+  requestAnimationFrame(animate);
+}
+animate();
+
+// box.addEventListener("mousemove", function(event){
+//     if (ball) {
+//           mouseX = event.pageX;
+//   mouseY = event.pageY;
+//     }else{
+//         ball.style.display='none';
+//     }
+// })
+box.addEventListener("mouseover", () => {
+  mouseX = event.pageX;
+  mouseY = event.pageY;
+})
+box.addEventListener('mouseout', (event) => {
+  // ball.style.display = 'none';
+      mouseX = -10;
+})
+
+
+
+
+
+
